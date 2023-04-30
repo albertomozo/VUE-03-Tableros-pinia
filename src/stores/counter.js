3b5412b1605e544652687a5881cfa2d4975f7478
Alberto <albertomozodocente@gmail.com>
2023-04-30 11:25:26 +0200

    counter.js a composition API

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,8 +1,28 @@
 // importamos defineStore
+import {computed, ref} from 'vue';
 import { defineStore } from "pinia"; 
 
+export const UseCounter = defineStore("counter", ()=>{
+  const count = ref(0);
+  const name = ref('Alberto');
+
+  const double = computed (() => { return count.value * 2  });
+  const nameUpper = computed(() => { return name.value.toUpperCase()});
+
+  function increment(){
+    count.value++;
+  }
+  function reverseName(){
+    const reverse = name.value.split('').reverse().join('');
+    name.value = reverse
+  }
+
+  
+  return {count,name,increment,reverseName, double, nameUpper}
+})
+
 // regla  const comenzar con UseNombre 
-export const UseCounter = defineStore ("Counter", {
+/* export const UseCounter = defineStore ("Counter", {
     //state 
     state : ()=>({
       count : 0,
@@ -24,4 +44,4 @@ export const UseCounter = defineStore ("Counter", {
       nameUpper : (state) => { return state.name.toUpperCase()}
     
     }
-})
+}) */
