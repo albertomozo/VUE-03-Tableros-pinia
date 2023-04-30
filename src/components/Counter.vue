86edf643d5d3ac5b76316cddccdd5785fba989f6
Alberto <albertomozodocente@gmail.com>
2023-04-30 09:47:04 +0200

    Paso 02 state, action y getters de la store

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,13 +1,20 @@
 <script setup>
     import {UseCounter} from "../stores/counter";
+    import { storeToRefs } from "pinia";
     const store = UseCounter();
+    const {name,count, double, nameUpper } = storeToRefs(store)
+    // vamoa a usar la funcion increment del store
+    const {increment, reverseName} = store;
 </script>
 <template>
     <div>
         <p>
-             Counter : {{ store.count }}
-        </p><p>
-             Name : {{ store.name }}
+             Counter : {{ count }}  {{ double  }}
         </p>
+        <p>
+             Name : {{ name }}   {{ nameUpper }}
+        </p>
+        <button @click="increment">Incrementar</button>
+        <button @click="reverseName">Invertir Name</button>
     </div>
 </template>
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,4 +1,4 @@
-// imnportamos defineStore
+// importamos defineStore
 import { defineStore } from "pinia"; 
 
 // regla  const comenzar con UseNombre 
@@ -7,8 +7,21 @@ export const UseCounter = defineStore ("Counter", {
     state : ()=>({
       count : 0,
       name : "Alberto"
-    })
-
+    }),
     // getter
     // actions
+    actions : {
+      increment(){
+        this.count++;
+      },
+      reverseName(){
+        const reverse = this.name.split('').reverse().join('');
+        this.name = reverse
+     }
+    },
+    getters : {
+      double : (state) => { return state.count * 2  },
+      nameUpper : (state) => { return state.name.toUpperCase()}
+    
+    }
 })
