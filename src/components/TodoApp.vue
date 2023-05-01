d9f02f55963f80b9f6729ab007a9439c8e8e2196
Alberto <albertomozodocente@gmail.com>
2023-05-01 08:41:25 +0200

    Todo04 Getters

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,10 +1,20 @@
 <script setup>
+    import {computed} from "vue"
+    import {useTodo} from "../stores/todo"
+    import {storeToRefs} from "pinia"
     import TodoForm from "./TodoForm.vue";
     import TodoList from "./TodoList.vue";
+    const store = useTodo()
+    const {totalTodo, totalCompleted} = storeToRefs(store)
+    const hasTodo = computed (() => {
+        return store.totalTodo >0 ? true : false;
+    })
 </script>
 
 <template>
-    <p>Todo App</p>
+    <h1>Todo </h1>
+    <p v-if="hasTodo">{{ `${totalCompleted} tareas completadas de ${totalTodo}` }}</p>
     <TodoForm />
     <TodoList />
+    <p v-if = "!hasTodo">Todavía no tienes ninguna tarea. Animate!!</p>
 </template>
